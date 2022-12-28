310e5bae4955a2ed39c206cbccaa75e4d99076fc
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-12-28 16:04:31 -0300

    Adiciona escutadores de eventos em todas as teclas e cria funções especiais para tab e caps

--- script.js
+++ script.js
@@ -1,13 +1,56 @@
 const keyboard = document.getElementById('keyboard-sec');
+const textArea = document.getElementById('text');
 const row1 = [...'\'1234567890-=', 'backspace'];
-const row2 = ['tab', ...'qwertyuiop[]', '\\'.length];
+const row2 = ['tab', ...'qwertyuiop[]', '\\'];
 const row3 = ['caps', ...'asdfghjkl;:','enter'];
 const row4 = ['shift', ...'zxcvbnm,./', 'shift'];
-const row5 = ['.com', '@', ''];
+const row5 = ['.com', '@', ' '];
+
+const upperChar = (key) => {
+  key.innerText = key.innerText.toUpperCase();
+  key.value = key.value.toUpperCase();
+};
+
+const lowerChars = (keys) => {
+  for (const key of keys) {
+    key.innerText = key.innerText.toLowerCase();
+    key.value = key.value.toLowerCase();
+  }
+};
+
+const canUpper = (key) => {
+  if (key.value.length <= 1) upperChar(key);
+};
+
+const caps = () => {
+  const keys = document.querySelectorAll('.row div');
+  if (keys[15].value === 'q') {
+    for (const key of keys) {
+      canUpper(key);
+    }
+  } else {
+    lowerChars(keys);
+  }
+};
+
+const sKeys = {
+  tab: () => textArea.value += '    ',
+  caps,
+};
+
+const chooseSpecialKeyFunc = (text) => {
+  return sKeys[text];
+};
+
+const genericKeyEvent = (event) => textArea.value += event.target.value;
+
+const chooseFunction = (text) => chooseSpecialKeyFunc(text) || genericKeyEvent;
 
 const creatKey = (keyText) => {
   const key = document.createElement('div');
   key.innerText = keyText;
+  key.value = keyText;
+  key.addEventListener('click', chooseFunction(keyText));
   return key;
 };
 
@@ -18,6 +61,6 @@ const creatSectionWKeys = (keys) => {
   return sec;
 };
 
-const concatAllRows = (...rows) => rows.map((row) => creatSectionWKeys(row));
+const concatAllRows = (...rows) => rows.map(creatSectionWKeys);
 
 keyboard.replaceChildren(...concatAllRows(row1, row2, row3, row4, row5));
